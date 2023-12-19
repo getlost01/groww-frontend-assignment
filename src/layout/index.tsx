f9c9caed2ba247476cd2b862e4ab5c3aa71c306e
Aagam Jain <79409258+getlost01@users.noreply.github.com>
2023-12-19 12:55:13 +0530

    add header and layout done

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,6 +1,12 @@
 import { ReactNode } from "react";
-import Header from "@/components/custom-components/Header";
 import { Public_Sans } from "next/font/google";
+// @mui
+import { Box, Grid } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+// Components
+import Header from "@/components/custom-components/Header";
+
+// ----------------------------------------------------------------------
 
 const primaryFont = Public_Sans({
   weight: ["400", "500", "600", "700", "800"],
@@ -8,17 +14,35 @@ const primaryFont = Public_Sans({
   fallback: ["Helvetica", "Arial", "sans-serif"],
 });
 
-// ----------------------------------------------------------------------
-
 type Props = {
   children: ReactNode;
 };
 
+// ----------------------------------------------------------------------
+
 export default function Layout({ children }: Props) {
+  const theme = useTheme();
+
   return (
-    <main className={`${primaryFont.className}`}>
+    <Box
+      className={`${primaryFont.className}`}
+      bgcolor={theme.palette.background.default}
+      color={theme.palette.text.primary}
+    >
+      {/* Header  */}
       <Header />
-      {children}
-    </main>
+
+      {/* Content  */}
+      <Grid
+        container
+        display="flex"
+        justifyContent="center"
+        sx={{ minHeight: "100vh" }}
+      >
+        <Grid item xs={12} sm={10} md={9} lg={8} p={1.5}>
+          {children}
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
