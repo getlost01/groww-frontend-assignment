7cae97ecf4331defc8d357bbe711c1e463cae24d
Aagam Jain <79409258+getlost01@users.noreply.github.com>
2023-12-20 00:58:06 +0530

    add change theme loader

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,10 +1,11 @@
 import { ReactNode } from "react";
 import { Public_Sans } from "next/font/google";
 // @mui
-import { Box, Grid } from "@mui/material";
+import { Box, Grid, CircularProgress, Typography } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 // Components
 import Header from "@/components/custom-components/Header";
+import useCustomTheme from "@/zustand/customTheme";
 
 // ----------------------------------------------------------------------
 
@@ -23,26 +24,53 @@ type Props = {
 export default function Layout({ children }: Props) {
   const theme = useTheme();
 
-  return (
-    <Box
-      className={`${primaryFont.className}`}
-      bgcolor={theme.palette.background.default}
-      color={theme.palette.text.primary}
-    >
-      {/* Header  */}
-      <Header />
+  const { isLoading } = useCustomTheme();
 
-      {/* Content  */}
-      <Grid
-        container
-        display="flex"
-        justifyContent="center"
-        sx={{ minHeight: "100vh" }}
-      >
-        <Grid item xs={12} sm={11} md={10} lg={8} p={1.5}>
-          {children}
-        </Grid>
-      </Grid>
-    </Box>
+  return (
+    <>
+      {isLoading ? (
+        <Box
+          sx={{
+            position: "fixed",
+            zIndex: 9999,
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: theme.palette.background.default,
+          }}
+        >
+          <CircularProgress size={60} thickness={3} />
+          <Typography variant="h6" sx={{ m: 2, color: "text.secondary" }}>
+            Fetching Theme...
+          </Typography>
+        </Box>
+      ) : (
+        <Box
+          className={`${primaryFont.className}`}
+          bgcolor={theme.palette.background.default}
+          color={theme.palette.text.primary}
+        >
+          {/* Header  */}
+          <Header />
+
+          {/* Content  */}
+          <Grid
+            container
+            display="flex"
+            justifyContent="center"
+            sx={{ minHeight: "100vh" }}
+          >
+            <Grid item xs={12} sm={11} md={10} lg={8} p={1.5}>
+              {children}
+            </Grid>
+          </Grid>
+        </Box>
+      )}
+    </>
   );
 }
